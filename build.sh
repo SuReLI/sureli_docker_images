4d775f8fd5e646d802fe75e9bf800ec9aed1e1b3
Carlos Aguilar <carlosaguilarmelchor@users.noreply.github.com>
2018-10-01 11:17:28 +0200

    minor change in build

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "#############################################\nBuilding SUReLI base image\n#############################################"
+echo -e "#############################################\nBuilding SUReLI base image\n#############################################"
 docker build -t sureli-docker-no-gym -f Dockerfile.base .
 if [ ! -f ./gym/README.rst ];
 then 
   echo "Gym does not seem to be installed stopping here"
   exit
 else
-  echo "#############################################\nBuilding SUReLI Gym image\n#############################################"
+  echo -e "#############################################\nBuilding SUReLI Gym image\n#############################################"
   docker build -t sureli-docker -f Dockerfile.addgym .
 fi
 
