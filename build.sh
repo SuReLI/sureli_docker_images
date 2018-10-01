d950cd09d511ee02eef8388f436a5038c6caa52c
Carlos Aguilar <carlosaguilarmelchor@users.noreply.github.com>
2018-10-01 13:42:39 +0200

    build message updated

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ do
   docker build -t sureli-docker-no-gym -f Dockerfile.base .
   if [ "$?" == "0" ]; then break; fi
 done
+echo -e "#############################################\nBuilding SUReLI complete\n#############################################"
 for i in `seq 1 10`
 do
   docker build -t sureli-docker -f Dockerfile.addgym .
