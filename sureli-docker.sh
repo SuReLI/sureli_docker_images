a9ea00a60e10d9082c93eaea7fd6cfcf5d4f91e2
Carlos Aguilar <carlosaguilarmelchor@users.noreply.github.com>
2018-10-17 14:04:23 +0200

    force tty allocation in ssh

--- sureli-docker.sh
+++ sureli-docker.sh
@@ -13,7 +13,7 @@ DOCKERNAME="sureli-docker"
 # Run the container
 docker run -it -d -p 127.0.0.1:38888:8888 -p 127.0.0.1:36006:6006 -p 127.0.0.1:32222:22 -v $(pwd)/notebooks:/notebooks $DOCKERNAME $@
 export containerId=$(docker ps -l -q)
-sshpass -p tutu ssh -o StrictHostKeyChecking=no -p 32222 -X root@127.0.0.1 tmux
+sshpass -p tutu ssh -o StrictHostKeyChecking=no -p 32222 -X -tt root@127.0.0.1 tmux
 
 # Stop it when we exit it
 docker stop $containerId
